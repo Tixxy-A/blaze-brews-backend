9a24c50af25802ed60748094f684e5536e6ff267
Tixxy-A <anujraut7221@gmail.com>
2024-03-07 02:05:36 +0530

    reset password added

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const cookieparser = require('cookie-parser');
-const User=require('./model/User')
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+//const sendgridtransport = require('nodemailer-sendgrid-transport');
+const User = require('./model/User')
 require('dotenv').config()
 
 const app = express()
@@ -18,8 +21,8 @@ app.use(cors({
 }))
 mongoose.connect(process.env.MONGOURL);
 
-app.get('/test',(req,res)=>{
-    res.json({message:"Hello World"})
+app.get('/test', (req, res) => {
+    res.json({ message: "Hello World" })
 })
 app.post('/register', async (req, res) => {
     try {
@@ -73,6 +76,66 @@ app.get('/profile', async (req, res) => {
         res.json(false);
     }
 });
+
+app.post('/reset-password', async (req, res) => {
+    const { email } = req.body;
+
+    // Create a Nodemailer transporter
+    const user = await User.findOne({ email: email });
+    //console.log(user);
+    if (user) {
+        const token = crypto.randomBytes(20).toString('hex');
+        //console.log(token);
+        user.resettoken = token;
+        user.tokenExpiration = Date.now() + 10 * 60 * 60;
+        user.save();
+        //console.log(user);
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'ajraut7221@gmail.com', // Your email address
+                pass: 'vkko qpur jiok pxvk' // Your email password (or app-specific password if using Gmail)
+            }
+        });
+
+        // Define email options
+        let mailOptions = {
+            from: 'ajraut7221@gmail.com', // Sender address
+            to: email, // List of recipients
+            subject: 'Reset your Password ', // Subject line
+            html:
+                '<p>Please click on the following link to reset your password:</p>' +
+                '<a href="http://localhost:3000/reset/' + token + '">click here</a>',
+        };
+
+        try {
+            // Send email
+            await transporter.sendMail(mailOptions);
+            res.status(200).send('Email sent successfully');
+        } catch (error) {
+            console.error('Error sending email:', error);
+            res.status(500).send('Error sending email');
+        }
+    } else {
+        res.status(404).json({ message: 'user not found' });
+    }
+
+});
+
+app.post('/new-password', async (req, res) => {
+    const { token, password } = req.body;
+    const user = await User.findOne({ resettoken: token });
+    if (user) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        user.password = hashedPassword;
+        user.save();
+        const token = jwt.sign({ email: user.email, id: user._id, name: user.name }, process.env.JWTSECRET);
+        res.cookie('token', token).json(user);
+    } else {
+        res.status(404).json({ message: 'invalid token' });
+    }
+})
 
 
 app.listen(3001, () => {
